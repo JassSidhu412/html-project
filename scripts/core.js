ef6f54e173c2bc537381ec988d336b55d2086502
JassSidhu412 <jasssidhu412@gmail.com>
2024-11-24 04:47:47 +0530

    Update core.js

--- scripts/core.js
+++ scripts/core.js
@@ -2,5 +2,8 @@ function getScripts(){
   return [
   'https://raw.githubusercontent.com/JassSidhu412/html-project/main/scripts/face-generator.js',
     'https://raw.githubusercontent.com/JassSidhu412/html-project/main/scripts/name-generator.js'
-  ]
+  ];
+}
+function test(){
+  alert("It works");
 }
