b392df1b6f9c7d8dc09892ff02ad37e8435dbf92
JassSidhu412 <jasssidhu412@gmail.com>
2024-11-24 20:34:56 +0530

    Update core.js

--- scripts/core.js
+++ scripts/core.js
@@ -1,10 +1,3 @@
-async function loadStyle(url) {
-    const response = await fetch(url);
-    const styleContent = await response.text();
-    const style = document.createElement('style');
-    script.textContent = styleContent;
-    document.head.appendChild(style);
-}
 async function fetchFile(url) {
     const response = await fetch(url);
     return await response.text();
@@ -12,12 +5,16 @@ async function fetchFile(url) {
 async function loadAllAssets() {
     let scriptAssets = ["face-generator", "name-generator", "input-box"];
     let styleAssets = ["input-box"];
+
     for (let scriptPath of scriptAssets) {
         const script = document.createElement('script');
         script.textContent = await fetchFile(`https://raw.githubusercontent.com/JassSidhu412/html-project/main/scripts/${scriptPath}.js`);
         document.body.appendChild(script);
     }
+
     for (let stylePath of styleAssets) {
-        await loadStyle(`https://raw.githubusercontent.com/JassSidhu412/html-project/main/styles/${stylePath}.css`);
+        const style = document.createElement('style');
+        style.textContent = await fetchFile(`https://raw.githubusercontent.com/JassSidhu412/html-project/main/styles/${stylePath}.css`);
+        document.head.appendChild(style);
     }
 }
