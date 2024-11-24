0110f17327dc31b5ca8624f8c3f8c767069991d0
JassSidhu412 <jasssidhu412@gmail.com>
2024-11-24 20:24:39 +0530

    Update core.js

--- scripts/core.js
+++ scripts/core.js
@@ -1,41 +1,28 @@
-let scriptAssets = ["face-generator", "name-generator", "input-box"];
-let styleAssets = ["input-box"];
-
-function getScripts() {
-    return [
-        'https://raw.githubusercontent.com/JassSidhu412/html-project/main/scripts/face-generator.js',
-        'https://raw.githubusercontent.com/JassSidhu412/html-project/main/scripts/name-generator.js'
-    ];
-}
-
-function test() {
-    alert("It works");
-}
-
-async function loadStyles(url) {
+async function loadStyle(url) {
     const response = await fetch(url);
     const styleContent = await response.text();
     const style = document.createElement('style');
     script.textContent = styleContent;
     document.head.appendChild(style);
 }
-async function getScript(url) {
+async function fetchFile(url) {
     const response = await fetch(url);
-    const scriptContent = await response.text();
-    return scriptContent;
+    return await response.text();
 }
 async function loadAllAssets() {
-    const scriptTag = document.createElement('div');
-    let st ='';
+    let scriptAssets = ["face-generator", "name-generator", "input-box"];
+    let styleAssets = ["input-box"];
+    const scriptTag = document.createElement('script');
+    let st = '';
     for (let scriptPath of scriptAssets) {
-        
-        st += await getScript(`https://raw.githubusercontent.com/JassSidhu412/html-project/main/scripts/${scriptPath}.js`);
+
+        st += await fetchFile(`https://raw.githubusercontent.com/JassSidhu412/html-project/main/scripts/${scriptPath}.js`);
     }
     scriptTag.textContent = st;
-scriptTag.id="hello";
+    scriptTag.id = "hello";
 
     document.body.appendChild(scriptTag);
-    
+
     for (let stylePath of styleAssets) {
         await loadStyle(`https://raw.githubusercontent.com/JassSidhu412/html-project/main/styles/${scriptPath}.css`);
     }
