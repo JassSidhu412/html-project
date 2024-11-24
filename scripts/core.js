e610bffe606b9064de3cceb133b7a877b217100b
JassSidhu412 <jasssidhu412@gmail.com>
2024-11-24 09:16:09 +0530

    Update core.js

--- scripts/core.js
+++ scripts/core.js
@@ -1,11 +1,12 @@
-function getScripts(){
-  return [
-  'https://raw.githubusercontent.com/JassSidhu412/html-project/main/scripts/face-generator.js',
-    'https://raw.githubusercontent.com/JassSidhu412/html-project/main/scripts/name-generator.js'
-  ];
+function getScripts() {
+    return [
+        'https://raw.githubusercontent.com/JassSidhu412/html-project/main/scripts/face-generator.js',
+        'https://raw.githubusercontent.com/JassSidhu412/html-project/main/scripts/name-generator.js'
+    ];
 }
-function test(){
-  alert("It works");
+
+function test() {
+    alert("It works");
 }
 
 async function loadStyle(url) {
@@ -15,14 +16,20 @@ async function loadStyle(url) {
     script.textContent = styleContent;
     document.head.appendChild(style);
 }
+async function getScript(url) {
+    const response = await fetch(url);
+    const scriptContent = await response.text();
+    return scriptContent;
+}
 async function loadAllAssets() {
     const fullPath = "https://raw.githubusercontent.com/JassSidhu412/html-project/main/scripts/dummy.js";
-
-    for (const scriptPath of ["face-generator", "name-generator","input-box"]) {
-        await loadScript(fullPath.replace('dummy', `${scriptPath}`));
+    const script = document.createElement('script');
+    for (const scriptPath of ["face-generator", "name-generator", "input-box"]) {
+        script.textContent += await getScript(fullPath.replace('dummy', `${scriptPath}`));
     }
+
+    document.body.appendChild(script);
     for (const stylePath of ["input-box"]) {
         await loadStyle(fullPath.replace(`scripts/dummy.js', 'styles/${scriptPath}.css`));
     }
 }
-
